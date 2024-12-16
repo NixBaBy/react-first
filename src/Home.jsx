1619faac757207f2d05c41844a6cdaf7a0ef2294
NixBaBy <tuuguu123123@gmail.com>
2024-12-16 11:52:04 +0800

    ehlew

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div class="asd">
+      <a href="#">
+        <p>menu</p>
+        <p>about</p>
+        <p>profile</p>
+      </a>
+    </div>
+  );
+};
+
+export default App;
