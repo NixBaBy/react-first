83686d14f406fff7e2ed34b8b4fc83f7557e5927
NixBaBy <tuuguu123123@gmail.com>
2024-12-16 12:44:11 +0800

    hicheel hiiw

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,43 @@
 import React from "react";
-
+import "./app.css";
 const App = () => {
   return (
-    <div class="asd">
-      <a href="#">
-        <p>menu</p>
-        <p>about</p>
-        <p>profile</p>
-      </a>
+    <div>
+      <Header />
+      <div className="cards">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+const Card = () => {
+  return (
+    <div className="img1">
+      <img
+        src="https://storage.googleapis.com/pod_public/1300/229900.jpg"
+        alt=""
+      />
+      <h2>Naruto</h2>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div class="header">
+      <div className="img">
+        <img
+          src="https://unread.today/files/site_meta/b809740220f5b5b4749e697e42df987b.png"
+          alt=""
+        />
+        <input type="text" placeholder="Hailt hiih" />
+      </div>
+      <div className="next-news">
+        <a href="#">daraagiin niitlel TECHWORM Margaash orno</a>
+      </div>
     </div>
   );
 };
